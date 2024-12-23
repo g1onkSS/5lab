63b8705c6acacb4496aa42b3c3a5cfa0c089681d
Artem Varlakov <kkg27466@gmail.com>
2024-12-23 15:18:35 +0500

    Добавлена разность чисел

--- main.cpp
+++ main.cpp
@@ -4,5 +4,11 @@ using namespace std;
 
 int main()
 {
-
+	int a, b;
+	std::cout << "Enter A and B: ";
+	std::cin >> a >> b;
+	std::cout << "A = " << a << '\n';
+	std::cout << "B = " << b << '\n';
+	std::cout << "A+B = " << a+b << '\n';
+	std::cout << "A-B = " << a-b << '\n';
 }
